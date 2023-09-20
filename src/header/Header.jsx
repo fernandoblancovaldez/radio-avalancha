b53269c8ae6c67efe2fcdcfe26f88f625e2ac36b
Fernando Blanco <fernandoblancovaldez@gmail.com>
2023-09-20 20:30:58 -0300

    adding title font

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -5,13 +5,19 @@ function Header() {
   return (
     <header>
       <Navbar className="d-flex align-items-center justify-content-center hdr-lgo-cont p-0 bg-blur shadow flex-column">
-        <Image src={AvalanchaLogo} alt="Avalancha Logo" className="h-25" />
+        <Image
+          src={AvalanchaLogo}
+          alt="Avalancha Logo"
+          className="avalancha-logo"
+        />
         <Col className="justify-content-center col-12 text-center">
-          <h1 className="h-50 mb-0 text-light fs-5">
-            <b className="fw-semibold lh-1">Escuchame entre el ruido</b>
+          <h1 className="mb-0 text-light fs-1 h-50">
+            <b className="fw-semibold">escuchame entre el ruido</b>
           </h1>
-          <h2 className="h-50 mb-0 text-light fs-6">
-            <em className="lh-1 fw-light">tu programa de rock</em>
+          <h2 className="mb-0 text-light fs-6">
+            <small>
+              <em className="lh-1 fw-light">- tu programa de rock -</em>
+            </small>
           </h2>
         </Col>
       </Navbar>
